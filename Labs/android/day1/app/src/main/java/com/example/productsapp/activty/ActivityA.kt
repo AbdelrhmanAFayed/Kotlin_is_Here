4e1fadebb06eae90954c31127ab5a772c01fd737
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-05-06 14:10:18 +0300

    Started on First day

---
+++ Labs/android/day1/app/src/main/java/com/example/productsapp/activty/ActivityA.kt
@@ -0,0 +1,13 @@
+package com.example.productsapp.activty
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.productsapp.R
+
+class ActivityA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+    }
+}
